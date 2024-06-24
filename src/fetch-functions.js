35107c24fcc2cf401bf63508e23ee4463a16fd43
Rashell Aldas <rashellaldas@rashells-mbp.mynetworksettings.com>
2024-06-24 03:24:50 -0400

    trying to understand it all

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,4 +1,23 @@
-export const getFirstThreeFantasyBooks = () => {
+export const getFirstThreeFantasyBooks = async () => {
+  // fetch data with URL
+  const url = ('https://openlibrary.org/subjects/fantasy.json')
+  const response = await fetch(url)
+  // check to see response is ok
+  try {
+    if (!response.ok) throw new Error(`Fetch failed to get fantasy books}`)
+
+
+    // parse response body to JSON into JS object 
+    const jsonData = await response.json()
+    console.log(jsonData)
+    // return 3 books
+    return jsonData.slice(0, 3)
+  }
+  catch (error) {
+    console.warn(error.message)
+    // return new promise (() => null)
+    return null
+  }
 };
 
 export const getAuthor = () => {
